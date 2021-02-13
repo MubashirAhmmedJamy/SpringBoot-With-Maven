62bf1fe473a9b728bb4f01d72bfe91a49d00c8f9
Jamy <mubashirjamy@gmail.com>
2021-02-13 13:48:19 +0600

    Spring boot with maven

---
+++ src/main/java/spring_boot_application/SpringBoot/Application.java
@@ -0,0 +1,15 @@
+package spring_boot_application.SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		
+		System.out.println("This is my first Spring Boot Application");
+	}
+
+}
---
+++ src/main/java/spring_boot_application/SpringBoot/BinarySearchImplementaion.java
@@ -0,0 +1,10 @@
+package spring_boot_application.SpringBoot;
+
+public class BinarySearchImplementaion {
+	
+	public int binarySearch(int [] a, int b){
+		System.out.println("whatever is given as an input we'll return 0");
+		return 0;
+	}
+	
+}
